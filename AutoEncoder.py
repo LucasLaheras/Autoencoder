6df8eb5efb0f097dddcb1ac8be2e5b99e8de510c
Lucas Laheras <lucaslaheras@hotmail.com>
2020-05-14 00:48:47 -0300

    AutoEncoder working

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -1,13 +1,10 @@
 import os
-import keras
 import numpy as np
 from keras.datasets import mnist
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras.callbacks import TensorBoard
-import tensorflow as tf
-from keras import backend as K
-
+from PIL import Image
 
 class ConvAutoEncoder:
 
@@ -22,29 +19,42 @@ class ConvAutoEncoder:
         self.input_shape = input_shape
         self.output_dim = output_dim
 
-        input_layer = keras.layers.Input(input_shape)
+        input_layer = Input(input_shape)
 
         x = Conv2D(filters[0], kernel, activation='relu', padding='same')(input_layer)
         x = MaxPooling2D((2, 2), padding='same')(x)
         x = Conv2D(filters[1], kernel, activation='relu', padding='same')(x)
         x = MaxPooling2D((2, 2), padding='same')(x)
         x = Conv2D(filters[2], kernel, activation='relu', padding='same')(x)
-        self.encoder = MaxPooling2D((2, 2), padding='same')(x)
+        encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-        # at this point the representation is (4, 4, 8) i.e. 128-dimensional
-
-        x = Conv2D(filters[2], kernel, activation='relu', padding='same')(self.encoder)
+        x = Conv2D(filters[2], kernel, activation='relu', padding='same')(encoded)
         x = UpSampling2D((2, 2))(x)
         x = Conv2D(filters[1], kernel, activation='relu', padding='same')(x)
         x = UpSampling2D((2, 2))(x)
         x = Conv2D(filters[0], kernel, activation='relu')(x)
         x = UpSampling2D((2, 2))(x)
-        self.decoder = Conv2D(1, kernel, activation='sigmoid', padding='same')(x)
+        decoded = Conv2D(1, kernel, activation='sigmoid', padding='same')(x)
 
-        reconstructed = self.decoder
+        # create autoencoder and decoder model
+        self.autoencoder = Model(input_layer, decoded)
+        self.encoder = Model(input_layer, encoded)
+
+        # create a placeholder for an encoded input
+        enc_shape = encoded[0].shape
+
+        encoded_input = Input(shape=(int(enc_shape[0]), int(enc_shape[1]), int(enc_shape[2])))
+
+        # retrieve the decoder layers and apply to each prev layer
+        num_decoder_layers = 7
+        decoder_layer = encoded_input
+        for i in range(-num_decoder_layers, 0):
+            decoder_layer = self.autoencoder.layers[i](decoder_layer)
+
+        # create the decoder model
+        self.decoder = Model(encoded_input, decoder_layer)
 
         # compile model
-        self.autoencoder = Model(inputs=input_layer, outputs=reconstructed)
         self.autoencoder.compile(optimizer=optimizer, loss=lossfn)
 
         self.autoencoder.summary()
@@ -67,13 +77,18 @@ class ConvAutoEncoder:
     def decode(self, codes):
         return self.decoder.predict(codes)
 
+    def autoencode(self, input):
+        return self.autoencoder.predict(input)
+
     def save_weights(self, path=None, prefix=""):
         if path is None: path = os.getcwd()
+        self.autoencoder.save_weights(os.path.join(path, prefix + "autoencoder_weights.h5"))
         self.encoder.save_weights(os.path.join(path, prefix + "encoder_weights.h5"))
         self.decoder.save_weights(os.path.join(path, prefix + "decoder_weights.h5"))
 
     def load_weights(self, path=None, prefix=""):
         if path is None: path = os.getcwd()
+        self.autoencoder.load_weights(os.path.join(path, prefix + "autoencoder_weights.h5"))
         self.encoder.load_weights(os.path.join(path, prefix + "encoder_weights.h5"))
         self.decoder.load_weights(os.path.join(path, prefix + "decoder_weights.h5"))
 
@@ -88,9 +103,24 @@ def configureDataset():
 
     return x_train, x_test
 
+
+def save_img(imgs, stri):
+    num = imgs.shape[0]
+
+    for i in range(num):
+        A = imgs[i].copy()*255
+        A = np.reshape(np.ravel(A), (28, 28))
+        new_p = Image.fromarray(A)
+        if new_p.mode != 'RGB':
+            new_p = new_p.convert('RGB')
+        new_p.save(os.path.join(stri, str(i) + ".jpg"))
+
+
 if __name__ == '__main__':
 
     x_train, x_test = configureDataset()
+
+    #save_img(x_test, 'IN/')
 
     auto = ConvAutoEncoder(x_train[0].shape, x_train[0].shape)
 
@@ -98,4 +128,11 @@ if __name__ == '__main__':
 
     auto.fit(x_train, x_test, epochs=1, batch_size=128, shuffle=True, validation_data=(x_test, x_test))
 
-    auto.save_weights("/content/drive/","mnist")
+    auto.save_weights(prefix="mnist_")
+
+    auto.load_weights(prefix="mnist_")
+
+    a = auto.encode(x_test)
+    b = auto.decode(a)
+
+    #save_img(b, 'OUT/')
